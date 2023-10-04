3107681808133a46da5c5fe3fd75832c5e43a1ff
Jerry Applegarth <applegarth1630@gmail.com>
2023-10-04 14:08:01 -0700

    Initial commit

---
+++ vite.config.cjs
@@ -0,0 +1,15 @@
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: 'index.html',
+				about: './about.html',
+
+				// ...
+				// List all files you want in your build
+			},
+		},
+	},
+});
